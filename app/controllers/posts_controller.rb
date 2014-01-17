1b25cdb01bfffba32fe150297f2c09a7869725dd
xinkui <xinkui@gmail.com>
2014-01-17 11:48:00 +0800

    test acl

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.accessible_by(current_ability)
   end
 
   # GET /posts/1
